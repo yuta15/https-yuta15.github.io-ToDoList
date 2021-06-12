d19c33d4e789da1cabb3793c5f80dfcfe38ae766
yuta15 <70703081+yuta15@users.noreply.github.com>
2021-06-13 02:51:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -29,17 +29,18 @@
   
   // フォーム入力、todo追加、
   document.getElementById('add-form').onsubmit = function(event) {
-    event.preventDefault();
+    let todoText = document.getElementById("todotext");
+    if(todoText.value === ""){
+      alert("タスクを入力してください。");
+      
+    }else{
+      event.preventDefault();
+      const li = `<li class = "dotask"><span class="time">${output}</span><span class="tasks" id = ${newTask}>${newTask}</span><i class="fas fa-trash-alt fa-fw fa-2x delete faa-shake animated-hover" id="fonta"></i></li>`;
+      document.getElementById('todos').insertAdjacentHTML('beforeend', li);
+      localStorage.setItem(`${newTask}`,`${li}`);
+    }
    
-    // 日付
-    const now = new Date();
-    const min = String(now.getMinutes()).padStart(2, '0');
-    const hour = now.getHours();
-    const day = now.getDate();
-    const month = now.getMonth() + 1;
-    const year = now.getFullYear();
-    const output = `${year}/${month}/${day} ${hour}:${min}`;
-   
+    
     // 追加したTODOを配列化
     const newTask = document.getElementById('add-form').add.value;
     // let todo = [];
@@ -49,10 +50,7 @@
     // }
     
     // リスト追加
-    const li = `<li class = "dotask"><span class="time">${output}</span><span class="tasks" id = ${newTask}>${newTask}</span><i class="fas fa-trash-alt fa-fw fa-2x delete faa-shake animated-hover" id="fonta"></i></li>`;
-    document.getElementById('todos').insertAdjacentHTML('beforeend', li);
      // lacalstrage保存
-     localStorage.setItem(`${newTask}`,`${li}`);
     //  console.log(localStorage.getItem(`${newTask}`));
 
 
