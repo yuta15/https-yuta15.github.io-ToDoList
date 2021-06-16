f91ebb6fd310737da63f9096374be495ec7c6c4c
yuta15 <70703081+yuta15@users.noreply.github.com>
2021-06-16 22:45:56 +0900

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,15 @@
     let returnTask;
     let add = document.getElementById('add-form');
     let submit = document.getElementById('submit');
-
+    
     // キーとLengthを受け取れば中身全部とってきてくれる関数。
     
+
     function b() {
       for(let i = 0; i < localStorage.length; i++) {
-        returnTask =  localStorage.getItem(i);
-        let firstLi1 = `<li class = "dotask"><span class="time">${output}</span><span class="tasks" id = ${i}>${returnTask}</span><i class="fas fa-trash-alt fa-fw fa-2x delete faa-shake animated-hover" id="fonta"></i></li>`;
+        let returnDataList =  JSON.parse(localStorage.getItem(i));
+        console.log(returnDataList);
+        let firstLi1 = `<li class = "dotask"><span class="time">${returnDataList.time}</span><span class="tasks" id = ${i}>${returnDataList.value}</span><i class="fas fa-trash-alt fa-fw fa-2x delete faa-shake animated-hover" id="fonta"></i></li>`;
         Up(firstLi1);
       }
     }
@@ -29,7 +31,7 @@
     }
     b();
 
-    
+  
   submit.addEventListener('click', e => {
     let form = document.forms[0].elements[0];
     let word = form.value;
@@ -40,7 +42,11 @@
       let i = localStorage.length;
       returnTask = word;
       let firstLi2 = `<li class = "dotask"><span class="time">${output}</span><span class="tasks" id = ${i}>${returnTask}</span><i class="fas fa-trash-alt fa-fw fa-2x delete faa-shake animated-hover" id="fonta"></i></li>`;
-      localStorage.setItem(`${i}`,`${returnTask}`);
+      let dataList =  {
+        value: returnTask,
+        time: output,
+      }
+      localStorage.setItem(i, JSON.stringify(dataList));
       Up(firstLi2);
       form.value = "";
     }
