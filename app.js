075238a4cbc5edd18f42f3dd36f084e1e77d4324
yuta15 <70703081+yuta15@users.noreply.github.com>
2021-06-16 23:12:58 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict'
 {
+  function getTime() {
     const now = new Date();
     const min = String(now.getMinutes()).padStart(2, '0');
     const hour = now.getHours();
@@ -7,12 +8,18 @@
     const month = now.getMonth() + 1;
     const year = now.getFullYear();
     const output = `${year}/${month}/${day} ${hour}:${min}`;
+    return output;
+  }
     const toDos = document.getElementById('todos');
     let returnKey;
     let returnTask;
     let add = document.getElementById('add-form');
     let submit = document.getElementById('submit');
-    
+    let dataList = {
+      value: "value",
+      time: "time",
+
+    }
     // キーとLengthを受け取れば中身全部とってきてくれる関数。
     
 
@@ -39,18 +46,17 @@
       alert("タスクを入力してください。");
     }else{
       e.preventDefault();
+      let returnTime = getTime();
       let i = localStorage.length;
       returnTask = word;
-      let firstLi2 = `<li class = "dotask"><span class="time">${output}</span><span class="tasks" id = ${i}>${returnTask}</span><i class="fas fa-trash-alt fa-fw fa-2x delete faa-shake animated-hover" id="fonta"></i></li>`;
-      let dataList =  {
-        value: returnTask,
-        time: output,
-      }
+      let firstLi2 = `<li class = "dotask"><span class="time">${returnTime}</span><span class="tasks" id = ${i}>${returnTask}</span><i class="fas fa-trash-alt fa-fw fa-2x delete faa-shake animated-hover" id="fonta"></i></li>`;
+      dataList.value = returnTask;
+      dataList.time = returnTime;
       localStorage.setItem(i, JSON.stringify(dataList));
       Up(firstLi2);
       form.value = "";
     }
-  }); 
+    }); 
 
 
 
